333fbc3ad863543e46520456aca38d9b5d3334ca
Sabarigirish Manikandan <sabari.girish2057@gmail.com>
2025-04-01 20:05:57 +0530

    directly capture picture from pi camera for cv

--- backend/cv_crowd.py
+++ backend/cv_crowd.py
@@ -1,23 +1,25 @@
+from os import system
+
+from picamzero import Camera
 from ultralytics import YOLO
 
 model = YOLO("yolov8x.pt")
 
 
-results = model.predict("backend/test.jpg")
+# results = model.predict("backend/test13.jpg")
+camera = Camera()
+system("mkdir /tmp/cv")
+camera.start_preview()
+camera.take_photo("/tmp/cv/test.jpg")
+camera.stop_preview()
+results = model.predict("/tmp/cv/test.jpg")
+system("rm /tmp/cv/test.jpg")
 result = results[0]
-# box = result.boxes[0]
-# cords = box.xyxy[0].tolist()
-# class_id = box.cls[0].item()
-# conf = box.conf[0].item()
-# print("Object type:", class_id)
-# print("Coordinates:", cords)
-# print("Probability:", conf)
-# print(result.names)
 people = 0
 for box in result.boxes:
     if (
-        box.cls[0] == 0 and box.conf[0] > 0.75
+        box.cls[0] == 0 # and box.conf[0] > 0.75
     ):  # TODO: tweak probability threshold with representative images
+        print(f" Person detected: {box.conf[0]}")
         people += 1
 print(f" Number of people detected: {people}")
-
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from campus-crowd-detection!")
-
-
-if __name__ == "__main__":
-    main()
